6d8523f7729a846c3b0e529dc58121aba9e8f00d
VINOTHSIVA <35728684+VINOTHSIVA@users.noreply.github.com>
2018-07-27 13:54:19 +0530

    Create oddorevennotusig%.c

---
+++ oddorevennotusig%.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter the value");
+    scanf("%d",&n);
+    if((n/2)*2==n)
+    {
+        printf("it is even");
+    }
+    else
+    {
+        printf("it is odd");
+    }
+}
